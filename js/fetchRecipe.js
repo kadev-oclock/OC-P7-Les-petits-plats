ac1ef7422baeac98c53e6fd26ce5da0d458f5fde
kadev <karimaoudiaa@gmail.com>
2023-10-03 22:10:48 +0200

    add card/header

--- js/fetchRecipe.js
+++ js/fetchRecipe.js
@@ -1,1 +1,15 @@
 // fetch le json
+
+// eslint-disable-next-line import/prefer-default-export
+export async function getRecipes() {
+  const reponse = await fetch("./data/recipes.json");
+  try {
+    if (!reponse.ok) {
+      throw new Error("pas de données json");
+    }
+    const data = await reponse.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 // traitement recherche / affichage des cards
+// eslint-disable-next-line import/extensions, import/no-unresolved
+import { getRecipes } from "./fetchRecipe.js";
 
-alert("coucou");
+async function init() {
+  const data = await getRecipes();
+  console.log("data:", data);
+}
+init();
